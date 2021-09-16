8c5199f29141525f3ee369e14651100c517f9f91
yuanshi <1075552122@qq.com>
2021-09-16 11:36:56 +0800

    使容器占满高度，若内容超过容器高度，则scroll

--- pages/test.vue
+++ pages/test.vue
@@ -1,18 +1,49 @@
 <template>
-  <div>
-    {{ o.a }}
+  <div class="wrap">
+    <div style="line-height: 30px">面包屑</div>
+    <div class="main-wrap">
+      <el-scrollbar>
+        <div class="aside">
+          <p>{{ "我是文字".repeat(199) }}</p>
+        </div>
+      </el-scrollbar>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      o: null,
-    };
-  },
-  beforeMount() {
-    this.o = { a: 1 };
-  },
-};
+export default {};
 </script>
+
+<style lang="scss" scoped>
+.wrap {
+  position: fixed;
+  top: 60px;
+  width: 100%;
+  height: calc(100vh - 60px);
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+}
+
+.main-wrap {
+  flex-grow: 1;
+  flex-shrink: 0;
+  display: flex;
+  // height: calc(100% - 30px);
+}
+
+.el-scrollbar {
+  min-width: 200px;
+  background-color: cornflowerblue;
+
+  ::v-deep .el-scrollbar__wrap {
+    overflow-x: hidden;
+  }
+}
+
+.aside {
+  width: 200px;
+  background-color: darkblue;
+}
+</style>
