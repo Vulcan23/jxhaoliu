c3597b24a34d0981e47c2d14e5bfa912af941089
yuanshi <1075552122@qq.com>
2021-09-18 10:13:26 +0800

    给项目配了个prettier

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,50 +1,50 @@
 export default {
-    target: "static",
-    plugins: [
-        "~/plugins/element-ui.js",
-        "~/plugins/vue-lazyload.js",
-        "~/plugins/route.js",
-        "~/plugins/swiper.js",
+  target: "static",
+  plugins: [
+    "~/plugins/element-ui.js",
+    "~/plugins/vue-lazyload.js",
+    "~/plugins/route.js",
+    "~/plugins/swiper.js",
+  ],
+  modules: ["@nuxtjs/axios", "@nuxtjs/proxy"],
+  proxy: {
+    "/s": {
+      target: "https://www.baidu.com",
+      changeOrigin: true,
+      secure: false,
+    },
+    "/api": {
+      target: "http://api.m.taobao.com",
+      changeOrigin: true,
+      secure: false,
+      pathRewrite: {
+        "^/api": "",
+      },
+    },
+  },
+  dev: process.env.NODE_ENV !== "production",
+  ssr: false,
+  head: {
+    title: "好六网络",
+    htmlAttrs: {
+      lang: "zh-CN",
+    },
+    meta: [
+      { charset: "utf-8" },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1, shrink-to-fit=no",
+      },
+      {
+        name: "keywords",
+        content: "好六网络,江西好六",
+      },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "江西好六网络科技有限公司（简称：好六网络），注册成立于2017年，是一家立足于新媒体头部流量整合，全面布局互联网广告营销生态企业。",
+      },
     ],
-    modules: ["@nuxtjs/axios", "@nuxtjs/proxy"],
-    proxy: {
-        "/s": {
-            target: "https://www.baidu.com",
-            changeOrigin: true,
-            secure: false,
-        },
-        "/api": {
-            target: "http://api.m.taobao.com",
-            changeOrigin: true,
-            secure: false,
-            pathRewrite: {
-                "^/api": "",
-            },
-        },
-    },
-    dev: process.env.NODE_ENV !== "production",
-    ssr: false,
-    head: {
-        title: "好六网络",
-        htmlAttrs: {
-            lang: "zh-CN",
-        },
-        meta: [
-            { charset: "utf-8" },
-            {
-                name: "viewport",
-                content: "width=device-width, initial-scale=1, shrink-to-fit=no",
-            },
-            {
-                name: "keywords",
-                content: "好六网络,江西好六",
-            },
-            {
-                hid: "description",
-                name: "description",
-                content:
-                    "江西好六网络科技有限公司（简称：好六网络），注册成立于2017年，是一家立足于新媒体头部流量整合，全面布局互联网广告营销生态企业。",
-            },
-        ],
-    },
+  },
 };
